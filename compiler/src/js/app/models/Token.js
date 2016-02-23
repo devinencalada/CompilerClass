edeb460987cf462f9e3e114e7f9e71d3dd6dfded
John Encalada <devinencalada@yahoo.com>
2016-02-22 22:27:16 -0500

    class to represent source code

---
+++ compiler/src/js/app/models/Token.js
@@ -0,0 +1,31 @@
+/**
+ * Class used to represent tokens founf in the source code.
+ * A token contains attributes to store the token type, code, token type name,
+ * regular expression used to identify the token and the line number of where the
+ * token is found in the code.
+ */
+ 
+ (function (Backbone, Compiler) {
+	 var Token = Backbone.Model.extend({
+		 
+		 defaults: {
+			 type: Token.T_NO_MATCH,
+			 code: null,
+			 name: null,
+			 regex: null,
+			 line: null
+		 },
+		 
+		 initialize: function(){
+			 this.setTokenAttributes();
+		 },
+		 
+		 /**
+		 * Checks this token's code is valid for the type of token type.
+		 *
+		 * @returns {boolean}
+		 */
+		 isValid: function() {
+			return this.get('regex').test(this.get('code'));
+		},
+ }
