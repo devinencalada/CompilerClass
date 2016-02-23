34093b37cdb8360a23d2bf934550135ecdc93d4b
John Encalada <devinencalada@yahoo.com>
2016-02-23 16:05:14 -0500

    Fixed logic error in comparison used to find a token

--- compiler/src/js/app/models/Token.js
+++ compiler/src/js/app/models/Token.js
@@ -4,37 +4,37 @@
  * regular expression used to identify the token and the line number of where the
  * token is found in the code.
  */
- 
- (function (Backbone, Compiler) {
-	
+
+(function (Backbone, Compiler) {
+
 	var Token = Backbone.Model.extend({
-		 
-		 defaults: {
-			 type: Token.T_NO_MATCH,
-			 code: null,
-			 name: null,
-			 regex: null,
-			 line: null
-		 },
-		 
-		 initialize: function(){
-			 this.setTokenAttributes();
-		 },
-		 
-		 /**
+
+		defaults: {
+			type: 0, // 'Default to T_NO_MATCH'
+			code: null,
+			name: null,
+			regex: null,
+			line: null
+		},
+
+		initialize: function() {
+			this.setTokenAttributes();
+		},
+
+		/**
 		 * Checks this token's code is valid for the type of token type.
 		 *
 		 * @returns {boolean}
 		 */
-		 isValid: function() {
+		isValid: function() {
 			return this.get('regex').test(this.get('code'));
 		},
-		
+
 		/**
 		 * Sets this model's token name and regular expression
 		 * attributes.
 		 */
-		 setTokenAttributes: function() {
+		setTokenAttributes: function() {
 			var attrs = {};
 
 			switch(this.get('type'))
@@ -149,7 +149,7 @@
 
 			this.set(attrs);
 		}
-		
+
 	}, {
 		/**
 		 * Token type constants
@@ -204,7 +204,7 @@
 
 				token.setTokenAttributes();
 
-				if (token.isValid())
+				if (token.get('regex') && token.isValid())
 				{
 					return token;
 				}
