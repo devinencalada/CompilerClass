04e565a78395901bbeaa1160a0440a544991ece9
John Encalada <devinencalada@yahoo.com>
2016-02-24 00:42:05 -0500

    Parser class definition

---
+++ compiler/src/js/app/Parser.js
@@ -0,0 +1,12 @@
+/**
+ * Class used to represent the Parser. The parser class analyzes
+ * The parser valid the code is made up of valid sentences.
+ */
+
+(function (Backbone, Compiler) {
+
+	var Parser = Backbone.Model.extend({
+
+	});
+
+});
