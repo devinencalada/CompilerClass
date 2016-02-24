022f9380b2c35d1bbe456b2603e740c7425c00eb
John Encalada <devinencalada@yahoo.com>
2016-02-24 01:00:34 -0500

    implemented method to return the next token

--- compiler/src/js/app/Parser.js
+++ compiler/src/js/app/Parser.js
@@ -12,8 +12,32 @@
 		 */
 		tokens: null,
 
+		/**
+		 * @property {Number} Index of the current token
+		 */
+		currentTokenIndex: 0,
+
 		initialize: function(tokens) {
 			this.tokens = tokens;
+		},
+
+		/**
+		 * Returns the next token in the token list.
+		 *
+		 * @returns {Compiler.Token}
+		 */
+		getNextToken: function() {
+			var nextTokenIndex = this.currentTokenIndex + 1;
+			if(nextTokenIndex > this.tokens.length)
+			{
+				return null;
+			}
+
+			// Update the current token's index
+			this.currentTokenIndex = nextTokenIndex;
+
+			// Return the token
+			return this.tokens[this.currentTokenIndex];
 		}
 	});
 
