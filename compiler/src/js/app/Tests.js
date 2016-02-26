22572927b10a96eb562e066121498e35b5b80892
John Encalada <devinencalada@yahoo.com>
2016-02-25 22:11:09 -0500

    Improved UI code to use the Event dispatcher for handling the display of logged errors and warnings

--- compiler/src/js/app/Tests.js
+++ compiler/src/js/app/Tests.js
@@ -8,47 +8,57 @@ $(document).ready(function() {
 		{ name: "If 3", code: "{\n\tint a\n\ta = 1\n\n\tif(a == 1) {\n\t\ta = 2\n\t}\n\n\tif(a != 1) {\n\t\ta = 3\n\t}\n} $" },
 		{ name: "While", code: "{\n\tint x\n\tx = 0\n\n\twhile (x != 5) \n\t{\n\t\tprint(x)\n\t\tx = 1 + x\n\t}\n} $" },
 		{ name: "Boolean", code: "{\n\tint a\n\ta = 1\n\n\tboolean b\n\tb = (true == (true != (false == (true != (false != (a == a))))))\n\n\tprint(b)\n} $" },
-		{ name: "Type Assignment Error", code: "{\n\tint 7\n\ta = 4\n\n\tint b\n\tb = 2 + a\n} $" },
-		{ name: "Boolean Error", code: "{\n\tint a\n\ta = 4\n\tif (a = 4) {\n\t\tprint(\"hello world\")\n\t}\n} $" },
-		{ name: "Unknown Lexeme Error", code: "{\n\tint a\n\ta = 1\n\n\tif(a == 1) {\n\t\ta = 2\n\t}\n\n\telse(a != 1) {\n\t\ta = 3\n\t}\n} $" },
-		{ name: "Missing Brace/Parenthesis Error", code: "{\n\tint a\n\ta = 4\n\n\tint b\n\tb = 2 + a\n $" },
-		{ name: "Integer over Digit Error", code: "{\n\tint a\n\ta = 42\n\n\tint b\n\tb = 2 + a\n} $" },
 	];
 
-	var validCodeTemplate = _.template($('#valid-code-template').text()),
-		invalidCodeTemplate = _.template($('#invalid-code-template').text()),
+	var outputTemplate = _.template($('#output-template').text()),
 		lexer = new Compiler.Lexer(),
 		parser = new Compiler.Parser(),
-		tokens = null;
+		tokens = null,
+		$outputElem = $("#output"),
+		$textAreaElem = $("textarea"),
+		$warningsLogElem = $("#warnings-log"),
+		$errorsLogElem = $("#errors-log");
 
+	// Populate the textarea when a selection has been made
+	// in the tests examples.
 	$("select").on("change", function(e) {
 		var value = $(this).val();
 		if(value !== "")
 		{
-			$("textarea").val(testCodeList[value].code);
+			$textAreaElem.val(testCodeList[value].code);
 		}
 	});
 
+	// Parse code once the form has been submitted
 	$("form").on("submit", function(e) {
 		e.preventDefault();
 
-		var source_code = $("textarea").val().trim();
+		// Reset output elements
+		$warningsLogElem.hide().find('pre').empty();
+		$errorsLogElem.hide().find('pre').empty();
+		$outputElem.empty();
+
+		// Get the source code from the textarea
+		var source_code = $textAreaElem.val().trim();
 		if(source_code == '')
 		{
 			alert('Please enter a code block.');
 			return;
 		}
 
+		// Parse the code
 		try
 		{
 			tokens = lexer.tokenize(source_code);
 		}
 		catch(err)
 		{
-			$("#output").empty().html(invalidCodeTemplate({
+			$outputElem.empty().html(invalidCodeTemplate({
 				source_code: source_code,
 				error: "Lexical " + err
 			}));
+
+			Compiler.dispatcher.trigger('log', 'error', "Lexical " + err);
 
 			return;
 		}
@@ -59,17 +69,30 @@ $(document).ready(function() {
 		{
 			parser.parse();
 
-			$("#output").empty().html(validCodeTemplate({
+			$outputElem.html(outputTemplate({
 				source_code: source_code,
 				tokens: tokens.toJSON()
-			}))
+			}));
 		}
 		catch(err)
 		{
-			$("#output").empty().html(invalidCodeTemplate({
-				source_code: source_code,
-				error: "Parsing " + err
-			}))
+			$outputElem.html(outputTemplate({
+				source_code: source_code
+			}));
+
+			Compiler.dispatcher.trigger('log', 'error', "Parsing " + err);
+		}
+	});
+
+	// Listen to the "log" event to update error and warning log elements
+	Compiler.dispatcher.on('log', function(type, message) {
+		if(type == 'warning')
+		{
+			$warningsLogElem.show().find('pre').append(" - " + message);
+		}
+		else
+		{
+			$errorsLogElem.show().find('pre').append(" - " + message);
 		}
 	});
 });
