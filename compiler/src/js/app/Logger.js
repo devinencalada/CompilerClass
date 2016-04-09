e9d100fd0e9a7940c68430ea823692b55a67e5fe
John Encalada <devinencalada@yahoo.com>
2016-04-09 01:21:07 -0400

    Implemented method used to return a count of log messages for an specified type and category

--- compiler/src/js/app/Logger.js
+++ compiler/src/js/app/Logger.js
@@ -31,6 +31,38 @@
 				type: type,
 				category: category
 			});
+		},
+
+		/**
+		 * Returns the count of messages for the specified type and category.
+		 *
+		 * @param {Int} type - Log message type
+		 * @param {Int} category - Log message category
+		 */
+		getCount: function(type, category) {
+			if(!type && !category)
+			{
+				return this.logs.length;
+			}
+			else if(type && !category)
+			{
+				return this.logs.where({
+					type: type
+				}).length;
+			}
+			else if(!type && category)
+			{
+				return this.logs.where({
+					category: category
+				}).length;
+			}
+			else
+			{
+				return this.logs.where({
+					type: type,
+					category: category
+				}).length;
+			}
 		}
 
 	}, {
