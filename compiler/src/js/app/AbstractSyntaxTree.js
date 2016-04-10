43dba2ac2a8aa2dbf72dee93a84311820be9335f
John Encalada <devinencalada@yahoo.com>
2016-04-10 02:16:39 -0400

    Implemented functionality to create an AST from a CST

--- compiler/src/js/app/AbstractSyntaxTree.js
+++ compiler/src/js/app/AbstractSyntaxTree.js
@@ -17,7 +17,202 @@
 		DIGIT_NODE: 'Digit',
 		EQUAL_NODE: 'Equal',
 		NOT_EQUAL_NODE: 'Not Equal',
-		BOOLEAN_EXPRESSION_NODE: 'Boolean Expression'
+		BOOLEAN_EXPRESSION_NODE: 'Boolean Expression',
+
+		isInvalidNode: function (node) {
+			var invalidNodes = ["=", "\"", "+", "(", ")", "print", "==", "!=", "if", "{", "}", "while"],
+				matched = false;
+
+			for (var i = 0; i < invalidNodes.length; i++) {
+				if (invalidNodes[i] === node) {
+					matched = true;
+					break;
+				}
+			}
+
+			return matched;
+		},
+
+		makeAST: function(cst) {
+			var ast = new Compiler.AbstractSyntaxTree();
+
+			function contains(node, desiredValue)
+			{
+				if (node !== null)
+				{
+					if (node.children.length === 0 && node.name == desiredValue)
+					{
+						return true;
+					}
+
+					var result = false,
+						childResult = false;
+
+					for (var i = 0; i < node.children.length; i++)
+					{
+						childResult = contains(node.children[i], desiredValue);
+
+						// Logical OR, so 1 or more occurrences of desired value results in true
+						result = result || childResult;
+					}
+
+					return result;
+				}
+			}
+
+			function traverse(node, interiorNodePath)
+			{
+				var wentDownALevel = true;
+
+				switch (node.name)
+				{
+					case Compiler.ConcreteSyntaxTree.BLOCK_NODE:
+						ast.addNode(Compiler.AbstractSyntaxTree.BLOCK_NODE, Compiler.AbstractSyntaxTree.BRANCH_NODE);
+						break;
+					case Compiler.ConcreteSyntaxTree.VAR_DECLARATION_NODE:
+						interiorNodePath = Compiler.AbstractSyntaxTree.VAR_DECLARATION_NODE;
+						ast.addNode(Compiler.AbstractSyntaxTree.VAR_DECLARATION_NODE, Compiler.AbstractSyntaxTree.BRANCH_NODE);
+						break;
+					case Compiler.ConcreteSyntaxTree.ASSIGNMENT_STATEMENT_NODE:
+						interiorNodePath = Compiler.AbstractSyntaxTree.ASSIGNMENT_STATEMENT_NODE;
+						ast.addNode(Compiler.AbstractSyntaxTree.ASSIGNMENT_STATEMENT_NODE, Compiler.AbstractSyntaxTree.BRANCH_NODE);
+						break;
+					case Compiler.ConcreteSyntaxTree.PRINT_STATEMENT_NODE:
+						interiorNodePath = Compiler.AbstractSyntaxTree.PRINT_STATEMENT_NODE;
+						ast.addNode(Compiler.AbstractSyntaxTree.PRINT_STATEMENT_NODE, Compiler.AbstractSyntaxTree.BRANCH_NODE);
+						break;
+					case Compiler.ConcreteSyntaxTree.IF_STATEMENT_NODE:
+						interiorNodePath = Compiler.AbstractSyntaxTree.IF_STATEMENT_NODE;
+						ast.addNode(Compiler.AbstractSyntaxTree.IF_STATEMENT_NODE, Compiler.AbstractSyntaxTree.BRANCH_NODE);
+						break;
+					case Compiler.ConcreteSyntaxTree.WHILE_STATEMENT_NODE:
+						interiorNodePath = Compiler.AbstractSyntaxTree.WHILE_STATEMENT_NODE;
+						ast.addNode(Compiler.AbstractSyntaxTree.WHILE_STATEMENT_NODE, Compiler.AbstractSyntaxTree.BRANCH_NODE);
+						break;
+					case Compiler.ConcreteSyntaxTree.INT_EXPRESSION_NODE:
+						// Add plus subtree
+						if(contains(node, '+'))
+						{
+							interiorNodePath = Compiler.AbstractSyntaxTree.ADD_NODE;
+							ast.addNode(Compiler.AbstractSyntaxTree.ADD_NODE, Compiler.AbstractSyntaxTree.BRANCH_NODE);
+						}
+						else
+						{
+							interiorNodePath = Compiler.AbstractSyntaxTree.DIGIT_NODE;
+						}
+
+						break;
+					case Compiler.ConcreteSyntaxTree.STRING_EXPRESSION_NODE:
+						interiorNodePath = Compiler.AbstractSyntaxTree.STRING_EXPRESSION_NODE;
+						//ast.addNode(Compiler.AbstractSyntaxTree.STRING_EXPRESSION_NODE, Compiler.AbstractSyntaxTree.LEAF_NODE);
+						break;
+
+					case Compiler.ConcreteSyntaxTree.CHAR_LIST_NODE:
+						var charList = '';
+						for (var i = 0; i < node.children.length; i++)
+						{
+							charList += node.children[i].name;
+						}
+
+						ast.addNode(charList, Compiler.AbstractSyntaxTree.LEAF_NODE);
+						break;
+
+					case Compiler.ConcreteSyntaxTree.BOOLEAN_EXPRESSION_NODE:
+						var comparisonOpFound = false;
+
+						for (var i = 0; i < node.children.length && !comparisonOpFound; i++)
+						{
+							var currentNode = node.children[i];
+
+							if (currentNode.name === "==")
+							{
+								interiorNodePath = Compiler.AbstractSyntaxTree.EQUAL_NODE;
+								ast.addNode(Compiler.AbstractSyntaxTree.EQUAL_NODE, Compiler.AbstractSyntaxTree.BRANCH_NODE);
+
+								comparisonOpFound = true;
+							}
+							else if (currentNode.name === "!=")
+							{
+								interiorNodePath = Compiler.AbstractSyntaxTree.NOT_EQUAL_NODE;
+								ast.addNode(Compiler.AbstractSyntaxTree.NOT_EQUAL_NODE, Compiler.AbstractSyntaxTree.BRANCH_NODE);
+
+								comparisonOpFound = true;
+							}
+						}
+
+						if (!comparisonOpFound)
+						{
+							interiorNodePath = Compiler.AbstractSyntaxTree.BOOLEAN_EXPRESSION_NODE;
+						}
+
+						break;
+
+					case Compiler.ConcreteSyntaxTree.STATEMENT_LIST_NODE:
+						interiorNodePath = null;
+						break;
+
+					default:
+						wentDownALevel = false;
+						break;
+				}
+
+
+				switch (interiorNodePath)
+				{
+					case Compiler.AbstractSyntaxTree.VAR_DECLARATION_NODE:
+					case Compiler.AbstractSyntaxTree.ASSIGNMENT_STATEMENT_NODE:
+					case Compiler.AbstractSyntaxTree.PRINT_STATEMENT_NODE:
+					case Compiler.AbstractSyntaxTree.ADD_NODE:
+					case Compiler.AbstractSyntaxTree.IF_STATEMENT_NODE:
+					case Compiler.AbstractSyntaxTree.WHILE_STATEMENT_NODE:
+					case Compiler.AbstractSyntaxTree.EQUAL_NODE:
+					case Compiler.AbstractSyntaxTree.NOT_EQUAL_NODE:
+						if (node.children.length == 0 && !Compiler.AbstractSyntaxTree.isInvalidNode(node.name))
+						{
+							ast.addNode(node.name, Compiler.AbstractSyntaxTree.LEAF_NODE);
+						}
+
+						break;
+
+					case Compiler.AbstractSyntaxTree.DIGIT_NODE:
+						if (node.children.length == 0 && !Compiler.AbstractSyntaxTree.isInvalidNode(node.name))
+						{
+							ast.addNode(node.name, Compiler.AbstractSyntaxTree.LEAF_NODE);
+						}
+
+						wentDownALevel = false;
+
+						break;
+
+					case Compiler.AbstractSyntaxTree.BOOLEAN_EXPRESSION_NODE:
+						if (node.children.length == 0 && !Compiler.AbstractSyntaxTree.isInvalidNode(node.name))
+						{
+							ast.addNode(node.name, Compiler.AbstractSyntaxTree.LEAF_NODE);
+						}
+
+						wentDownALevel = false;
+
+						break;
+
+					default:
+						break;
+				}
+
+				for (var i = 0; i < node.children.length; i++)
+				{
+					traverse(node.children[i], interiorNodePath);
+				}
+
+				if (wentDownALevel)
+				{
+					ast.endChildren();
+				}
+			}
+
+			traverse(cst.root, null);
+
+			return ast;
+		}
 	});
 
 	Compiler.AbstractSyntaxTree = Tree;
