305e96cb1a6bfc4ca6b060cdcd0a3d22f09a517f
John Encalada <devinencalada@yahoo.com>
2016-04-10 14:38:20 -0400

    Set up class structure for the Tree nodes

---
+++ compiler/src/js/app/models/TreeNode.js
@@ -0,0 +1,15 @@
+/**
+ * Classed used to represent nodes of the AST and CST
+ */
+
+(function (Backbone, Compiler) {
+
+	var TreeNode = Backbone.Model.extend({
+
+	});
+
+	Compiler.TreeNode = TreeNode;
+
+})(Backbone, Compiler);
+
+// Set up class structure for the Tree nodes
