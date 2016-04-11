aca47c711908d503c51a05259616caedd0b025db
John Encalada <devinencalada@yahoo.com>
2016-04-11 17:42:58 -0400

    Added default attributes to the SymbolTableEntry model

--- compiler/src/js/app/models/SymbolTableEntry.js
+++ compiler/src/js/app/models/SymbolTableEntry.js
@@ -6,7 +6,14 @@
 (function (Backbone, Compiler) {
 
 	var SymbolTableEntry = Backbone.Model.extend({
-
+		defaults: {
+			entry_number: -1,
+			name: '',
+			type: '',
+			line: -1,
+			scope: -1,
+			references: 0
+		}
 	});
 
 	Compiler.SymbolTableEntry = SymbolTableEntry;
