1ed541c8c259abf790ff9a430919b84758d55b1b
John Encalada <devinencalada@yahoo.com>
2016-04-11 18:12:16 -0400

    Fixed it

--- compiler/src/js/app/ScopeTable.js
+++ compiler/src/js/app/ScopeTable.js
@@ -5,11 +5,32 @@
 (function (Backbone, Compiler) {
 
 	var ScopeTable = Backbone.Model.extend({
+		/**
+		 * @property {Compiler.SymbolTableEntry[]} entries - List of symbol entries
+		 */
+		entries: null,
 
+		/**
+		 * @property {Int} nextEntryIndex - Next available symbol entry index
+		 */
+		nextEntryIndex: 0,
+
+		/**
+		 * @property {Int} scopeLevel - Current scope level
+		 */
+		scopeLevel: -1,
+
+		/**
+		 * @property {Int} parentScope - Parent scope level
+		 */
+		parentScope: null,
+
+		/**
+		 * @property {Array} childScopes - List of child scopes
+		 */
+		childScopes: null
 	});
 
 	Compiler.ScopeTable = ScopeTable;
 
 })(Backbone, Compiler);
-
-// Set up class structure for the scope table
