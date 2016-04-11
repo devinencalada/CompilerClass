73c19432f49e2242bdf449f42d41a98a20218b57
John Encalada <devinencalada@yahoo.com>
2016-04-11 17:59:22 -0400

    beginning to establish symbol table

---
+++ compiler/src/js/app/ScopeTable.js
@@ -0,0 +1,15 @@
+/**
+ * Classed used to represent the scope table.
+ */
+
+(function (Backbone, Compiler) {
+
+	var ScopeTable = Backbone.Model.extend({
+
+	});
+
+	Compiler.ScopeTable = ScopeTable;
+
+})(Backbone, Compiler);
+
+// Set up class structure for the scope table
