88f2f91055b8da0627bbc48dac533e8dfd6c3a85
John Encalada <devinencalada@yahoo.com>
2016-04-11 17:40:21 -0400

    Set up class structure for the entries of the symbol table

---
+++ compiler/src/js/app/models/SymbolTableEntry.js
@@ -0,0 +1,14 @@
+/**
+ * Classed used to represent entries in the
+ * symbol table.
+ */
+
+(function (Backbone, Compiler) {
+
+	var SymbolTableEntry = Backbone.Model.extend({
+
+	});
+
+	Compiler.SymbolTableEntry = SymbolTableEntry;
+
+})(Backbone, Compiler);
