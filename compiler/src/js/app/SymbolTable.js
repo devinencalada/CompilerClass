e9abced91faf33f56ab28cc1a5639637b314f784
John Encalada <devinencalada@yahoo.com>
2016-04-11 22:16:27 -0400

    Added wrapper methods to SymbolTable.addEntry, SymbolTable.hasEntry and SymbolTable.detectWarnings

--- compiler/src/js/app/SymbolTable.js
+++ compiler/src/js/app/SymbolTable.js
@@ -23,6 +23,37 @@
 		initialize: function() {
 			this.defaultScopeTable = new Compiler.ScopeTable();
 			this.currentScopeTable = this.defaultScopeTable;
+		},
+
+		/**
+		 * Adds an entry to the symbol table.
+		 *
+		 * @param {Compiler.SymbolTableEntry} symbolTableEntry
+		 * @returns {Boolean}
+		 */
+		addEntry: function(symbolTableEntry) {
+			return this.currentScopeTable.addEntry(symbolTableEntry);
+		},
+
+		/**
+		 * Checks if an entry is in the symbol table; if it is, it links the node
+		 * to that symbol table entry.
+		 *
+		 * @param {String} name
+		 * @param {Compiler.TreeNode} node
+		 * @param {String} optionalPath
+		 *
+		 * @returns {Boolean}
+		 */
+		hasEntry: function(name, node, optionalPath) {
+			return this.currentScopeTable.hasEntry(name, node, optionalPath);
+		},
+
+		/**
+		 * Detects the variable warnings
+		 */
+		detectWarnings: function() {
+			this.defaultScopeTable.detectWarnings();
 		}
 	});
 
