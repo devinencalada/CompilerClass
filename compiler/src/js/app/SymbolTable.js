ec94ba424edae19a3f378310df371480264c9bd4
John Encalada <devinencalada@yahoo.com>
2016-04-11 21:57:57 -0400

    Set up class structure for the symbol table

---
+++ compiler/src/js/app/SymbolTable.js
@@ -0,0 +1,13 @@
+/**
+ * Classed used to represent the symbol table.
+ */
+
+(function (Backbone, Compiler) {
+
+	var SymbolTable = Backbone.Model.extend({
+
+	});
+
+	Compiler.SymbolTable = SymbolTable;
+
+})(Backbone, Compiler);
