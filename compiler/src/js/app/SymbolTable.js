4abc4c3dc9d2718dca21883a4a7b5e6717953401
John Encalada <devinencalada@yahoo.com>
2016-04-11 22:05:49 -0400

    Added initialization code for the Symbol Table

--- compiler/src/js/app/SymbolTable.js
+++ compiler/src/js/app/SymbolTable.js
@@ -5,7 +5,25 @@
 (function (Backbone, Compiler) {
 
 	var SymbolTable = Backbone.Model.extend({
+		/**
+		 * @property {Compiler.ScopeTable} defaultScopeTable
+		 */
+		defaultScopeTable: null,
 
+		/**
+		 * @property {Compiler.ScopeTable} currentScopeTable
+		 */
+		currentScopeTable: null,
+
+		/**
+		 * @property {Int} nextScopeNumber
+		 */
+		nextScopeNumber: 0,
+
+		initialize: function() {
+			this.defaultScopeTable = new Compiler.ScopeTable();
+			this.currentScopeTable = this.defaultScopeTable;
+		}
 	});
 
 	Compiler.SymbolTable = SymbolTable;
