a8cfa086a5d64e56690617bd347e376e6c239a3f
John Encalada <devinencalada@yahoo.com>
2016-04-14 02:48:13 -0400

    Fixed error in condition by checking for the existance of the token property of a tree node.

--- compiler/src/js/app/SymbolTable.js
+++ compiler/src/js/app/SymbolTable.js
@@ -50,7 +50,7 @@
 			else
 			{
 				var errorMessage = 'Error! Attempt was made to move to non existent parent scope.';
-				Compiler.Logger.log(errorMessage, Compiler.Logger.ERROR, Compiler.Logger.SCOPE_TABLE);
+				Compiler.Logger.log(errorMessage, Compiler.Logger.ERROR, Compiler.Logger.SEMANTIC_ANALYSIS);
 				throw errorMessage;
 			}
 		},
@@ -86,6 +86,92 @@
 		 */
 		getWarnings: function() {
 			return this.defaultScopeTable.getWarnings();
+		}
+	}, {
+
+		/**
+		 * Builds the symbol using an AST.
+		 *
+		 * @param {Compiler.AbstractSyntaxTree} ast
+		 *
+		 * @returns {Compiler.SymbolTable}
+		 */
+		makeSymbolTable: function(ast) {
+			var symbolTable = new Compiler.SymbolTable();
+
+			function traverse(node, symbolTable)
+			{
+				var newScope = false,
+					optionalPath = '';
+
+				switch(node.name)
+				{
+					case Compiler.AbstractSyntaxTree.BLOCK_NODE:
+						symbolTable.openScope();
+						newScope = true;
+						break;
+
+					case Compiler.AbstractSyntaxTree.VAR_DECLARATION_NODE:
+						var name = node.children[1].name,
+							type = node.children[0].name,
+							line = node.children[1].token.get('line');
+
+						var insertResult = symbolTable.addEntry(Compiler.SymbolTableEntry.createEntry(name, type, line));
+
+						optionalPath = Compiler.AbstractSyntaxTree.VAR_DECLARATION_NODE;
+
+						if(!insertResult)
+						{
+							var errorMessage = 'Error! Duplicate declaration of id ' + name + ' found on line ' + line + '.';
+							Compiler.Logger.log(errorMessage, Compiler.Logger.ERROR, Compiler.Logger.SEMANTIC_ANALYSIS);
+							throw errorMessage;
+						}
+
+						break;
+
+					case Compiler.AbstractSyntaxTree.ASSIGNMENT_STATEMENT_NODE:
+						var name = node.children[0].name,
+							line = node.children[0].token.get('line');
+
+						var result = symbolTable.hasEntry(node.children[0].name, node, Compiler.AbstractSyntaxTree.ASSIGNMENT_STATEMENT_NODE);
+						if(!result)
+						{
+							var errorMessage = 'Error! The id ' + name + ' on line ' + line + ' was used before being declared.';
+							Compiler.Logger.log(errorMessage, Compiler.Logger.ERROR, Compiler.Logger.SEMANTIC_ANALYSIS);
+							throw errorMessage;
+						}
+
+						break;
+				}
+
+				if(node.token && node.token.get('type') === Compiler.Token.T_ID)
+				{
+					var name = node.name,
+						line = node.token.get('line');
+
+					var result = symbolTable.hasEntry(name, node, optionalPath);
+					if(!result)
+					{
+						var errorMessage = 'Error! The id ' + id + ' on line ' + line + ' was used before being declared.';
+						Compiler.Logger.log(errorMessage, Compiler.Logger.ERROR, Compiler.Logger.SEMANTIC_ANALYSIS);
+						throw errorMessage;
+					}
+				}
+
+				for(var i = 0; i < node.children.length; i++)
+				{
+					traverse(node.children[i], symbolTable);
+				}
+
+				if(newScope)
+				{
+					symbolTable.closeScope();
+				}
+			}
+
+			traverse(ast.root, symbolTable);
+
+			return symbolTable;
 		}
 	});
 
