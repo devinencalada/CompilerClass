3babe20f2fb87fa73a12d3a1271ad5301fab260e
John Encalada <devinencalada@yahoo.com>
2016-04-14 01:33:26 -0400

    Set up class structure for the semantic analyzer

---
+++ compiler/src/js/app/SemanticAnalyzer.js
@@ -0,0 +1,13 @@
+/**
+ * Classed used to represent the semantic analyzer.
+ */
+
+(function (Backbone, Compiler) {
+
+	var SemanticAnalyzer = Backbone.Model.extend({
+
+	});
+
+	Compiler.SemanticAnalyzer = SemanticAnalyzer;
+
+})(Backbone, Compiler);
