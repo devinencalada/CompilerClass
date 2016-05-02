a03e847c007f2cd9f5d8eed03916c09eaaa2b9ee
John Encalada <devinencalada@yahoo.com>
2016-05-02 01:56:26 -0400

    Moved initialization code to the AssemblyCode collection

--- compiler/src/js/app/CodeGenerator.js
+++ compiler/src/js/app/CodeGenerator.js
@@ -22,32 +22,15 @@
 		assemblyCode: null,
 
 		/**
-		 * @property {Number} curr
-		 */
-		currIndex: 1,
-
-		/**
-		 * @property {Number} heap
-		 */
-		heapPointer: 1,
-
-		/**
 		 * @property {Compiler.AbstractSyntaxTree} ast
 		 */
 		ast: null,
 
 		initialize: function(ast) {
 			this.ast = ast;
-			this.currIndex = 0;
-			this.heapPointer = Compiler.CodeGenerator.MAX_CODE_SIZE;
 			this.assemblyCode = new Compiler.AssemblyCode();
 			this.jumpTable = new Compiler.JumpTable();
 			this.tempJumpTable = new Compiler.TempJumpTable();
-
-			for (var i = 0; i < Compiler.CodeGenerator.MAX_CODE_SIZE; i++)
-			{
-				this.assemblyCode.add(Compiler.CodeGenerator.NO_CODE);
-			}
 		}
 
 	}, {
