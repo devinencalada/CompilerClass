765bc748dc117ee5db9336c02bc465557cdb1cc4
John Encalada <devinencalada@yahoo.com>
2016-05-02 01:01:43 -0400

    Created collection for the jump table

---
+++ compiler/src/js/app/collections/JumpTable.js
@@ -0,0 +1,15 @@
+/**
+ * Collection used to represent the jump table
+ */
+
+(function (Backbone, Compiler) {
+
+	var JumpTable = Collection.Model.extend({
+
+	});
+
+	Compiler.JumpTable = JumpTable;
+
+})(Backbone, Compiler);
+
+// Created collection for the jump table
