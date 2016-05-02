f9954ab77727cb5d508839d96f1c7ecaaa4a8332
John Encalada <devinencalada@yahoo.com>
2016-05-02 01:55:43 -0400

    Moved Assembly code and Jump table logic to their respective collections

--- compiler/src/js/app/CodeGenerator.js
+++ compiler/src/js/app/CodeGenerator.js
@@ -7,17 +7,17 @@
 	var CodeGenerator = Backbone.Model.extend({
 
 		/**
-		 * @property {Backbone.Collection} tempJumpTable
+		 * @property {Compiler.TempJumpTable} tempJumpTable
 		 */
 		tempJumpTable: null,
 
 		/**
-		 * @property {Backbone.Collection} jumpTable
+		 * @property {Compiler.JumpTable} jumpTable
 		 */
 		jumpTable: null,
 
 		/**
-		 * @property {Backbone.Collection} assemblyCode
+		 * @property {Compiler.AssemblyCode} assemblyCode
 		 */
 		assemblyCode: null,
 
@@ -40,75 +40,13 @@
 			this.ast = ast;
 			this.currIndex = 0;
 			this.heapPointer = Compiler.CodeGenerator.MAX_CODE_SIZE;
-			this.assemblyCode = Backbone.Collection();
-			this.jumpTable = Backbone.Collection();
-			this.tempJumpTable = Backbone.Collection();
+			this.assemblyCode = new Compiler.AssemblyCode();
+			this.jumpTable = new Compiler.JumpTable();
+			this.tempJumpTable = new Compiler.TempJumpTable();
 
 			for (var i = 0; i < Compiler.CodeGenerator.MAX_CODE_SIZE; i++)
 			{
 				this.assemblyCode.add(Compiler.CodeGenerator.NO_CODE);
-			}
-		},
-
-		/**
-		 * Creates and adds to the jump table
-		 * an instance of Compiler.JumpTableEntry.
-		 *
-		 * @returns {Compiler.JumpTableEntry}
-		 */
-		insertJumpTableEntry: function() {
-
-			var entry = new Compiler.JumpTableEntry({
-				temp_name: 'J' + this.jumpTable.length.toString()
-			});
-
-			this.jumpTable.add(entry);
-
-			return entry;
-		},
-
-		/**
-		 * Creates and adds to the temp jump table
-		 * an instance of Compiler.TempJumpTableEntry.
-		 *
-		 * @returns {Compiler.TempJumpTableEntry}
-		 */
-		insertTempJumpTableEntry: function() {
-
-			var entry = new Compiler.TempJumpTableEntry({
-				temp_name: 'T' + this.tempJumpTable.length.toString(),
-				address_offset: this.tempJumpTable.length
-			});
-
-			this.tempJumpTable.add(entry);
-
-			return entry;
-		},
-
-		/**
-		 * Returns an entry from the temp jump table by
-		 * the specified id name and scope level.
-		 *
-		 * @param {String} id - ID name
-		 * @param {Number} scope - Scope level
-		 * @returns {Compiler.TempJumpTable}
-		 */
-		getTempJumpTableEntry: function(id, scope) {
-
-			var entry = this.tempJumpTable.findWhere({
-				id_name: id,
-				scope: scope
-			});
-
-			if (entry)
-			{
-				return entry;
-			}
-			else
-			{
-				var errorMessage = 'Error! Id ' + id + ' was not found in the temp table.';
-				Compiler.Logger.log(errorMessage, Compiler.Logger.ERROR, Compiler.Logger.CODE_GENERATOR);
-				throw errorMessage;
 			}
 		}
 
