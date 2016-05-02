1c356132882cd1b94bdc5d18fa5141c94739ecad
John Encalada <devinencalada@yahoo.com>
2016-05-02 01:07:20 -0400

    Implemented initialization code

--- compiler/src/js/app/collections/AssemblyCode.js
+++ compiler/src/js/app/collections/AssemblyCode.js
@@ -6,7 +6,20 @@
 
 	var AssemblyCode = Collection.Model.extend({
 
-		model: Compiler.AssemblyCodeEntry
+		model: Compiler.AssemblyCodeEntry,
+
+		heapPointer: 0,
+
+		currIndex: 0,
+
+		initialize: function() {
+			this.heapPointer = Compiler.CodeGenerator.MAX_CODE_SIZE;
+
+			for (var i = 0; i < Compiler.CodeGenerator.MAX_CODE_SIZE; i++)
+			{
+				this.add({code: Compiler.CodeGenerator.NO_CODE});
+			}
+		}
 	});
 
 	Compiler.AssemblyCode = AssemblyCode;
