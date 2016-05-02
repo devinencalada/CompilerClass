3a11a7d1c339a64e91218e6bbe536cffce960e12
John Encalada <devinencalada@yahoo.com>
2016-05-01 23:30:58 -0400

    Set up initialization code for the code generator

--- compiler/src/js/app/CodeGenerator.js
+++ compiler/src/js/app/CodeGenerator.js
@@ -6,8 +6,53 @@
 
 	var CodeGenerator = Backbone.Model.extend({
 
+		/**
+		 * @property {Backbone.Collection} tempJumpTable
+		 */
+		tempJumpTable: null,
+
+		/**
+		 * @property {Backbone.Collection} jumpTable
+		 */
+		jumpTable: null,
+
+		/**
+		 * @property {Backbone.Collection} assemblyCode
+		 */
+		assemblyCode: null,
+
+		/**
+		 * @property {Number} curr
+		 */
+		currIndex: 1,
+
+		/**
+		 * @property {Number} heap
+		 */
+		heapPointer: 1,
+
+		/**
+		 * @property {Compiler.AbstractSyntaxTree} ast
+		 */
+		ast: null,
+
+		initialize: function(ast) {
+			this.ast = ast;
+			this.currIndex = 0;
+			this.heapPointer = Compiler.CodeGenerator.MAX_CODE_SIZE;
+			this.assemblyCode = Backbone.Collection();
+			this.jumpTable = Backbone.Collection();
+			this.tempJumpTable = Backbone.Collection();
+
+			for (var i = 0; i < Compiler.CodeGenerator.MAX_CODE_SIZE; i++)
+			{
+				this.assemblyCode.add(Compiler.CodeGenerator.CODE_PLACEHOLDER);
+			}
+		}
+
 	}, {
-		MAX_CODE_SIZE: 256
+		MAX_CODE_SIZE: 256,
+		CODE_PLACEHOLDER: "00"
 	});
 
 	Compiler.CodeGenerator = CodeGenerator;
