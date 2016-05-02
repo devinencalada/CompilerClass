36ebc7b38c81dae8bbdd91de96dbc2df6396ac33
John Encalada <devinencalada@yahoo.com>
2016-05-01 23:17:49 -0400

    Set up class structure for the code generator

---
+++ compiler/src/js/app/CodeGenerator.js
@@ -0,0 +1,13 @@
+/**
+ * Classed used to represent the code generator.
+ */
+
+(function (Backbone, Compiler) {
+
+	var CodeGenerator = Backbone.Model.extend({
+
+	});
+
+	Compiler.CodeGenerator = CodeGenerator;
+
+})(Backbone, Compiler);
